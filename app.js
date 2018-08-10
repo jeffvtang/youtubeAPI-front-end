3f08db2bf6cb3a9a49c372f6285d74239843fe33
Jeff <jeff.tang@live.com>
2018-08-10 01:16:52 -0400

    created templates for pages in views and router

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var dataRouter = require('./routes/data');
+var analyticsRouter = require('./routes/analytics');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/data', dataRouter);
+app.use('/analytics', analyticsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
