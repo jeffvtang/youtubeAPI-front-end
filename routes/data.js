14edeb60c853efbdea87a92bf86eff800767fcfc
Jeff <jeff.tang@live.com>
2018-08-11 20:22:05 -0400

    experimenting with sample code

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('data', { title: 'Data' });
+  res.render('upload', { title: 'Data' });
 });
 
 module.exports = router;
