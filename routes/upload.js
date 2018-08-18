a08f951c04662cc071c08bbf4a69215fd080ff28
Jeff <jeff.tang@live.com>
2018-08-18 15:25:10 -0400

    added chart for views over time

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('upload', { title: 'Data' });
+  res.render('upload', { title: 'Upload' });
 });
 
 module.exports = router;
